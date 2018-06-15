b686baa7450c67747d7e93ead76d106a849a6c62
ernestrlee <30732731+ernestrlee@users.noreply.github.com>
2018-06-15 02:25:01 -0700

    Added return values and imaging

--- readImageFiles.py
+++ readImageFiles.py
@@ -1,27 +1,38 @@
 from PIL import Image
+import numpy as np
 import glob
 import matplotlib.pyplot as plt
-
-# Images obtained from AT&T Laboratories Cambridge
-# http://www.cl.cam.ac.uk/research/dtg/attarchive/facedatabase.html
-
-image_list = []
-dataFiles = r"C:\Users\MKC\Documents\UCSC training\Machine Learning\Team project\att_faces\*\*.pgm"
 
 # This function changes vector data into an image plot
 # v - the vector data
 # show - boolean whether to show the plot or not
 def vectortoimg(v,show=True):
-    plt.imshow(v,interpolation='None', cmap='gray')
+    plt.imshow(v.reshape(112, 92),interpolation='None', cmap='gray')
     plt.axis('off')
     if show:
         plt.show()
 
+# Plot 100 images from the data
+# X - 100 samples of the data set (i.e. [0:100])
+def draw100VectorImages(X):
+    print("Checking multiple training vectors by plotting images.\nBe patient:")
+    plt.close('all')
+    fig = plt.figure()
+    nrows=10
+    ncols=10
+    currentImage=0
+    for row in range(nrows):
+        for col in range(ncols):
+            plt.subplot(nrows, ncols, row*ncols+col + 1)
+            vectortoimg(X[currentImage],show=False)
+            currentImage += 1
+    plt.show()
+
 # This function reads in an image, converts it into grayscale and
-# decodes it into a represented matrix of integers, and stores it into a list
+# decodes it into a represented array of integers, and stores each image into a list
 # dataFiles - the path of the data files
-# imageList - the list to append the data to
-def createImageList(dataFiles, imageList):
+def createImageList(dataFiles):
+    imageList = []
     for filename in glob.glob(dataFiles):
         img = Image.open(filename).convert("L")
         WIDTH, HEIGHT = img.size
@@ -34,11 +45,20 @@ def createImageList(dataFiles, imageList):
         # Convert image data to a list of integers
         data = list(img.getdata()) 
         # Convert that to 2D list (list of lists of integers)
-        data = [data[offset:offset+WIDTH] for offset in range(0, WIDTH*HEIGHT, WIDTH)]
+        #data = [data[offset:offset+WIDTH] for offset in range(0, WIDTH*HEIGHT, WIDTH)]
+        
         # Append the data to the list    
         imageList.append(data)
+    
+    return imageList
 
-
-createImageList(dataFiles, image_list)
-# For drawing an image
-# vectortoimg(image_list[10])
+# ------- For debugging -------
+# Images obtained from AT&T Laboratories Cambridge
+# http://www.cl.cam.ac.uk/research/dtg/attarchive/facedatabase.html
+# dataFiles = r"C:\Users\MKC\Documents\UCSC training\Machine Learning\Team project\att_faces\*\*.pgm"
+# X = np.array(createImageList(dataFiles))
+# print(X.shape)
+# For drawing an individual image
+# vectortoimg(X[10])
+# For drawing 100 images
+# draw100VectorImages(X[300:])
